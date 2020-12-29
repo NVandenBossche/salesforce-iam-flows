9e1518ff9ffbc8e757268b7f1a4295eee3dcebad
Nicolas Vanden Bossche <DyingStar@users.noreply.github.com>
2020-12-29 22:20:58 +0100

    Fixed hardcoding in queryresult.js + comments

--- client/js/queryresult.js
+++ client/js/queryresult.js
@@ -1,68 +1,78 @@
-var $apiEle = $("#apiCount"),
-    proxyURL = "https://localhost:8081/proxy/";
+// Initialize global variables and instantiate the Salesforce communication client based on cookies
+var apiCount = 0,
+    proxyURL = 'https://localhost:8081/proxy/',
+    instanceUrl = $.cookie('InstURL'),
+    apiVersion = $.cookie('APIVer'),
+    accessToken = $.cookie('AccToken'),
+    idUrl = $.cookie('idURL'),
+    clientId = '',
+    client = new forcetk.Client(clientId, instanceUrl, proxyURL);
 
-var client = new forcetk.Client("", 'https://nicolasvandenbossche-dev-ed.my.salesforce.com', proxyURL);
-
-if ($.cookie("AccToken") && $.cookie("APIVer") && $.cookie("InstURL")) {
-    client.setSessionToken(
-        $.cookie("AccToken"),
-        $.cookie("APIVer"),
-        $.cookie("InstURL")
-    );
+// Set the access token for the client's session, display the user info, and execute the query
+if (accessToken && apiVersion && instanceUrl) {
+    client.setSessionToken(accessToken, apiVersion, instanceUrl);
     getLoggedInUserInfo();
     executeQuery();
 } else {
-    $("#result").html(
+    $('#result').html(
         'Some issue occurred during authentication. Please contact admin or try again by <a href="index.html">navigating here</a>.'
     );
 }
 
-$("#Query-to-execute").keypress(function(e) {
-    var key = e.which;
+// Execute the query and display results if the user presses the 'Enter' button on their keyboard while the query box has focus.
+$('#Query-to-execute').keypress(function (e) {
+    let key = e.which;
+
+    // 13 is the key code for the 'Enter' key
     if (key == 13) {
-        // the enter key code
         executeQuery();
     }
 });
 
+// Retrieve details of the logged in user and display their name
 function getLoggedInUserInfo() {
-    var url = $.cookie("idURL");
+    let requestPayLoad = '',
+        retry = true;
 
     client.ajax(
-        url,
-        function(data) {
-            $("#loggedInUser").html(data.display_name);
+        idUrl,
+        function (data) {
+            let nameOfUser = data.display_name;
+            $('#loggedInUser').html(nameOfUser);
         },
-        function(error) {
+        function (error) {
             console.log(error);
         },
-        "GET",
-        "",
-        true
+        'GET',
+        requestPayLoad,
+        retry
     );
 }
 
+// Execute the query that's entered in the text box
 function executeQuery() {
+    // Check if the user already has a session. If not, display error. If yes, display the result.
     if (!client.sessionId) {
-        $("#result").html("You are not authenticated. Please login first.");
-        return false;
+        $('#result').html('You are not authenticated. Please login first.');
+    } else {
+        let queryToExecute = $('#Query-to-execute').val();
+
+        client.query(
+            queryToExecute,
+            function (data) {
+                let replacer = null,
+                    whitespace = 4;
+                $('#result').html(JSON.stringify(data, replacer, whitespace));
+            },
+            function (error) {
+                $('#result').html('Error: ' + JSON.stringify(error));
+            }
+        );
     }
-    client.query(
-        $("#Query-to-execute").val(),
-        function(data) {
-            $("#result").html(JSON.stringify(data, undefined, 3));
-        },
-        function(error) {
-            $("#result").html("Error: " + JSON.stringify(error));
-        }
-    );
-    return false;
 }
 
+// Count the number of API calls since this page was loaded (just a gimmick)
 function addAPICount() {
-    if (!$apiEle) {
-        $apiEle = $("#apiCount");
-    }
-
-    $apiEle.text(parseInt($apiEle.text()) + 1);
+    apiCount++;
+    $('#apiCount').text(apiCount);
 }
