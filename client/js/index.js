57c94eaae53b052b3836f58a2cab9f6c7c8e7770
Nicolas Vanden Bossche <DyingStar@users.noreply.github.com>
2021-01-01 22:26:45 +0100

    Moved JavaScript from index page to separate file

---
+++ client/js/index.js
@@ -0,0 +1,30 @@
+// Clear all cookies upon the loading of the page
+function clearAllCookies() {
+    $.cookie('AccToken', '');
+    $.cookie('APIVer', '');
+    $.cookie('InstURL', '');
+    $.cookie('idURL', '');
+    $.cookie('LoggeduserId', '');
+}
+
+// Single method for launching any of the flows
+function launchFlow(flowName, isSandbox, type) {
+    // Initialize the redirect URL
+    let newLocation = flowName + '?isSandbox=' + isSandbox;
+
+    // Add parameters to the URL where needed
+    if (type) {
+        newLocation += '&type=' + type;
+    }
+    if (flowName === 'jwt') {
+        newLocation += '&jwtUserName=' + $('#jwtUserName').val();
+    }
+
+    window.location = newLocation;
+}
+
+function onload() {
+    clearAllCookies();
+}
+
+window.onload = onload;
